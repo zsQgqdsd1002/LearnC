eb77f9546f2466ecb12aff562fda84437979d33d
zsQgqdsd1002 <1393268496@qq.com>
2021-06-08 12:23:20 +0800

    一些练习和非递归实现斐波那契数列

---
+++ 20210605.c
@@ -0,0 +1,124 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<math.h>
+//long long Fib(int n) {
+//	if (n == 1 || n == 2) {
+//		return 1;
+//	}
+//	else {
+//		return Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+//int main() {
+//	int a;
+//	scanf("%d", &a);
+//	long long b = Fib(a);
+//	printf("%lld", b);
+//	return 0;
+//}
+
+
+//long long Jump(int n) {
+//	if ( n == 1){
+//		return 1;
+//}
+//	else if (n == 2) {
+//		return 2;
+//	}
+//	else {
+//		return Jump(n - 1) + Jump(n - 2);
+//	}
+//}
+//int main() {
+//	int a;
+//	scanf("%d", &a);
+//	long long b = Jump(a);
+//	printf("%lld", b);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int Fun(n) {
+//    int a = 2 << n - 1;
+//    return a;
+//}
+//
+//int main() {
+//    int a;
+//    while (scanf("%d", &a) != EOF) {
+//        printf("%d\n", Fun(a));
+//    }
+//    return 0;
+//}
+//int main() {
+//    int n, m, h,a;
+//    scanf("%d %d %d", &n, &m, &h);
+//    a = h / m;
+//    if (h % m == 0) {
+//        a = h / m;
+//    }
+//    else {
+//        a = h / m + 1;
+//    }
+//    printf("%d",n - a);
+//    return 0;
+//}
+
+// int main() {
+    // int i, j;
+    // int k = 0;
+    // for (i = 100; i < 1000; i++) {
+        // for (j = 2; j < sqrt(i); j++)
+        // {
+            // if (i % j == 0) 
+                // break;
+            // }
+            // if(j >= sqrt(i)){
+                // k += 1;
+            // }  
+    // }
+    // printf("%d", k);
+    // return 0;
+// }
+// int main() {
+    // int i, j;
+    // int k = 0;
+    // for (i = 100; i < 1000; i++) {
+        // for (j = 2; j < sqrt(i); j++)
+        // {
+            // if (i % j == 0)
+                // break;
+        // }
+        // if (j >= sqrt(i))
+        // {
+            // k += 1;
+        // }
+    // }
+    // printf("%d", k);
+    // return 0;
+// }
+
+long long Fib(n) {
+	long long f1 = 1;
+	long long f2 = 1;
+	long long f3 = 0;
+	if (n <= 0) {
+		return -1;
+	}
+	if (n == 1 || n == 2) {
+		return 1;
+	}
+	for (int i = 3; i <= n; i++) {
+		f3 = f1 + f2;
+		f1 = f2;
+		f2 = f3;
+	}
+	return f3;
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	long long ret = Fib(n);
+	printf("%lld", ret);
+	return 0;
+}
